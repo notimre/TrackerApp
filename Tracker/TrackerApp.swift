a0106ec876791f3fba83319930c5d0db29275a7f
notimre <draskovits.imre@gmail.com>
2022-01-11 22:45:14 +0000

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Imre Draskovits on 11/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
