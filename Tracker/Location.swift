b6f2aa1271972fe12a9b194d26c68ae4923fb1b8
notimre <draskovits.imre@gmail.com>
2022-01-12 00:06:58 +0000

    Added media & nav bars

---
+++ Tracker/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  Tracker
+//
+//  Created by Imre Draskovits on 11/01/2022.
+//
+
+import Foundation
+
+struct Location {
+    let id: Int
+    let name: String
+    let country: String
+    let description: String
+    let more: String
+    let latitude: Double
+    let longitude: Double
+    let heroPicture: String
+    let advisory: String
+}
